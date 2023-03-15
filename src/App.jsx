b048b4c26f0eecae65e9c48b8460914cbf7b1158
jscastroavatar <39204657+jscastroavatar@users.noreply.github.com>
2023-03-15 00:00:12 -0500

    Creación lista cryptomonedas, axios y css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import "./App.css"
 
 function App() {
 
+  const API_URL = import.meta.env.VITE_API_URL
+  const [criptos, setCriptos] = useState()
+
+  useEffect(()=> {
+    axios.get(`${API_URL}assets`) //llama url
+    .then((data) => {
+      setCriptos(data.data.data) //ingresa a la data
+    })
+    .catch(() => {
+      console.error("La petición falló")
+    })
+  }, []) 
+
+  if (!criptos) return <span>Cargando...</span> //para que no se vea lento o pantalla blanca en conexiones lentas
+
   return (
-    <h1>Hola</h1>
+    <>
+    <h1>Lista de criptomonedas</h1>
+    <ol>
+      { 
+        criptos.map(({id, name, priceUsd}) =>( //listar el nombre y precio
+          <li key={id}>Nombre: {name} Precio: {priceUsd}</li>
+        ))
+      }
+    </ol>
+      
+    </>
+    
   )
 }
 
